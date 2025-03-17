265de4797890ae3879e2a28a599c0fbd15403396
ghxstling <dcho282@aucklanduni.ac.nz>
2025-03-18 00:37:04 +1300

    refactor(wip): use dynamic data, tweaked structure and style

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { GalleryVerticalEnd } from "lucide-react";
 
 import {
   Sidebar,
@@ -12,167 +11,23 @@ import {
   SidebarMenuSub,
   SidebarMenuSubButton,
   SidebarMenuSubItem,
-  SidebarRail,
 } from "@/components/ui/sidebar";
+import { SidebarContentData } from "@/data/sidebar-content";
 
-// This is sample data.
-const data = {
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Community",
-      url: "#",
-      items: [
-        {
-          title: "Contribution Guide",
-          url: "#",
-        },
-      ],
-    },
-  ],
-};
-
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({
+  data,
+  ...props
+}: React.ComponentProps<typeof Sidebar> & { data: SidebarContentData }) {
   return (
     <Sidebar {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <a href="#">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
-                </div>
-              </a>
-            </SidebarMenuButton>
+            <h4
+              className={`flex justify-center text-2xl font-semibold tracking-wide`}
+            >
+              {data.name}
+            </h4>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
@@ -181,16 +36,18 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenu>
             {data.navMain.map((item) => (
               <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <a href={item.url} className="font-medium">
-                    {item.title}
-                  </a>
+                <SidebarMenuButton
+                  disabled
+                  asChild
+                  className="cursor-default hover:bg-transparent"
+                >
+                  <p className="font-semibold text-[1rem]">{item.title}</p>
                 </SidebarMenuButton>
                 {item.items?.length ? (
                   <SidebarMenuSub>
                     {item.items.map((item) => (
                       <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                        <SidebarMenuSubButton asChild isActive={item.active}>
                           <a href={item.url}>{item.title}</a>
                         </SidebarMenuSubButton>
                       </SidebarMenuSubItem>
@@ -202,7 +59,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarRail />
     </Sidebar>
   );
 }
---
+++ src/data/sidebar-content.tsx
@@ -0,0 +1,219 @@
+export type SidebarContentData = {
+  name: string;
+  navMain: {
+    title: string;
+    items: { title: string; url: string; active: boolean }[];
+  }[];
+};
+
+export const CPU: SidebarContentData = {
+  name: "CPUs",
+  navMain: [
+    {
+      title: "CPU Family",
+      items: [
+        {
+          title: "AMD Ryzen 3",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD Ryzen 5",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD Ryzen 7",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD Ryzen 9",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel Core i3",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel Core i5 / Ultra 5",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel Core i7 / Ultra 7",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel Core i9 / Ultra 9",
+          url: "#",
+          active: false,
+        },
+      ],
+    },
+    {
+      title: "CPU Generation",
+      items: [
+        {
+          title: "AMD Ryzen 3000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD Ryzen 5000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD Ryzen 7000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD Ryzen 9000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel 12th Generation",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel 13th Generation",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel 14th Generation",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel Core Ultra 200",
+          url: "#",
+          active: false,
+        },
+      ],
+    },
+    {
+      title: "CPU Socket",
+      items: [
+        {
+          title: "AMD AM4",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD AM5",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel LGA1700",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Intel LGA1851",
+          url: "#",
+          active: false,
+        },
+      ],
+    },
+  ],
+};
+
+export const GPU: SidebarContentData = {
+  name: "Graphics Cards",
+  navMain: [
+    {
+      title: "GPU Family",
+      items: [
+        {
+          title: "NVIDIA RTX 3000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "NVIDIA RTX 4000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "NVIDIA RTX 5000 Series",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD RX 5000",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD RX 7000",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "AMD RX 9000",
+          url: "#",
+          active: false,
+        },
+      ],
+    },
+    {
+      title: "Brand",
+      items: [
+        {
+          title: "ASUS",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Gigabyte",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "MSI",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "GALAX",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "PNY",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Zotac",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Asrock",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Powercolor",
+          url: "#",
+          active: false,
+        },
+        {
+          title: "Sapphire",
+          url: "#",
+          active: false,
+        },
+      ],
+    },
+  ],
+};
