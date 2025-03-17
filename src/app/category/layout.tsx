e48d04f72e95db4ba648c9bb95f5fc92e724cb25
ghxstling <dcho282@aucklanduni.ac.nz>
2025-03-18 00:32:42 +1300

    feat(wip): move layout file to /category route, refactor layout  refactor category layout to include Header and Card components

---
+++ src/app/category/layout.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Card } from "@/components/ui/card";
+import { CPU } from "@/data/sidebar-content";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      <SidebarProvider>
+        <div className="flex flex-row gap-10 my-auto items-center justify-center">
+          <Card className="w-fit h-[60rem] max-h-[70vh] overflow-auto bg-neutral-50 dark:bg-neutral-900 border-neutral-400 dark:border-neutral-700">
+            <AppSidebar data={CPU} />
+          </Card>
+          {children}
+        </div>
+      </SidebarProvider>
+    </section>
+  );
+}
