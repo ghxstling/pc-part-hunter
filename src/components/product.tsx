57af923370892d893755a622403e3ac4dbf41b39
ghxstling <dcho282@aucklanduni.ac.nz>
2025-03-17 16:02:24 +1300

    feat: add Link component to Product

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -2,21 +2,27 @@
 
 import * as React from "react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const styles = {
-  button: "w-60 h-72 bg-neutral-200 border-neutral-400 hover:bg-neutral-300",
+  button:
+    "w-60 h-72 bg-neutral-200 border-neutral-400 hover:bg-neutral-300 cursor-pointer",
   content: "flex flex-col items-center gap-6",
 };
 
 export default function Product({
   children,
+  link,
   ...props
 }: {
   children: React.ReactNode;
+  link: string;
 }) {
   return (
-    <Button variant={"outline"} className={styles.button} {...props}>
-      <div className={styles.content}>{children}</div>
-    </Button>
+    <Link href={link}>
+      <Button variant={"outline"} className={styles.button} {...props}>
+        <div className={styles.content}>{children}</div>
+      </Button>
+    </Link>
   );
 }
