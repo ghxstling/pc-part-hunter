d9b1f1820c0e5018539ae70e9510a5959c43eeea
ghxstling <dcho282@aucklanduni.ac.nz>
2025-03-19 19:29:03 +1300

    feat: add 404 not found page

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -12,9 +12,16 @@ export default function RedirectPage() {
     setLoaded(true);
 
     if (loaded) {
-      router.push("/");
+      setTimeout(() => {
+        router.push("/");
+      }, 1000);
     }
   }, [router, loaded, setLoaded]);
 
-  return null;
+  return (
+    <div className="fixed flex flex-col gap-1 top-0 left-0 w-full h-full z-50 flex flex-col justify-center items-center gap-2 bg-white text-black dark:bg-neutral-950 dark:text-white text-center">
+      <p>Woah, how&#39;d you get here?</p>
+      <p>Taking you home ...</p>
+    </div>
+  );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Product from "@/components/product";
 
@@ -5,7 +7,7 @@ export default function NotFound() {
   return (
     <div className="fixed top-0 left-0 w-full h-full z-50 flex flex-col justify-center items-center gap-2 bg-white text-black dark:bg-neutral-950 dark:text-white">
       <code className="text-9xl">404</code>
-      <code className="text-2xl tracking-[0.1rem]">Page Not Found</code>
+      <code className="text-2xl">Page doesn&#39;t exist... yet(?)</code>
 
       <Product link={"/"} className="mt-[10vh] text-md">
         Return Home
